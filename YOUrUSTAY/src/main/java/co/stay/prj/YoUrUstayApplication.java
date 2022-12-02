3741a455de078cc917ea191d8bbc6285286e749a
SOHYEON <hellozzlol62@naver.com>
2022-12-02 19:52:45 +0900

    20221202첫커밋

---
+++ YOUrUSTAY/src/main/java/co/stay/prj/YoUrUstayApplication.java
@@ -0,0 +1,21 @@
+package co.stay.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class YoUrUstayApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(YoUrUstayApplication.class, args);
+	}
+
+}
